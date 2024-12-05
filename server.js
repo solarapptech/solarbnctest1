16b3748c3f356ea4be445907d6e68039e77e4168
tenubar <solartech4756@gmail.com>
2024-12-04 21:03:04 -0400

    Added latest updates

--- server.js
+++ server.js
@@ -6,26 +6,28 @@ server.keepAliveTimeout = 80000;
 server.headersTimeout = 81000;
 app.use(cors())
 
+let tasabinance = 0;
+let bncv = 0;
 
 const { spawn } = require('child_process');
 
 const childPython = spawn('python',['apibnc.py']);
 
 childPython.stdout.on('data',(data)=>{
-    // const tasabinance = data;
-    const tasabinance = `${data}`;
+    tasabinance = `${data}`;
+    bncv = tasabinance.trim();
+    launchinfo();
 })
 
-
+function launchinfo(){
 app.get ('/info5', (req, res) =>{
-    res.setHeader('Content-Type', 'text/event-stream')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-
-    const sendData5 = `data: ${JSON.stringify(tasabinance) +' Bs.'}\n\n`;
-    console.log(sendData5);
-    // res.write(sendData5);
-
+        res.setHeader('Content-Type', 'text/event-stream')
+        res.setHeader('Access-Control-Allow-Origin', '*')
+    
+        const sendData5 = `data: ${bncv +' Bs.'}\n\n`;
+        res.write(sendData5);
 })
+}
 
 childPython.stderr.on('data',(data)=>{
     console.error(`stderr: ${data}`);
